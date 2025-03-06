01de90dacc986652a6df9de2bada5511cab5542e
abfiker <new94abemelek@gmail.com>
2025-03-06 10:51:30 +0300

    cors added

--- app.ts
+++ app.ts
@@ -5,12 +5,15 @@ import dotenv from 'dotenv'
 import { connectDB } from './config/db';
 import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found.error';
+import cors from 'cors'
 // satys routes
 import StaysRoutes from './routes/stayRoutes'
 dotenv.config();
 connectDB()
 export const port = process.env.PORT || 3030
 const app = express();
+// CORS should be before any routes or other middleware that needs to access the request
+app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
